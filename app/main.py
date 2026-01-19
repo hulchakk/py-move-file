e043f1f513545b84feca3592d32d29b9cbe57a57
Dmytro <ys33ys33ys55@gmail.com>
2026-01-19 11:16:30 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,27 @@
-# write your code here
+import os
+
+
+def move_file(command_line: str) -> None:
+    command = command_line.split()
+    if command[0] != "mv" or len(command) != 3:
+        return
+
+    source_file = command[1]
+    target = command[2].split("/")
+    target_name = target[-1]
+    target_path = ""
+    target.pop(-1)
+    for dir_name in target:
+        target_path = target_path + f"{dir_name}/"
+        try:
+            os.mkdir(target_path)
+        except FileExistsError:
+            target_path = target_path
+
+    with open(source_file, "r") as file:
+        files_content = file.read()
+        os.remove(source_file)
+
+    target_path = target_path + target_name
+    with open(target_path, "w") as file:
+        file.write(files_content)
